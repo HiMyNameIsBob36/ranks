0041a2023f794329c1b9ebcd5fd6a2abf2677e6c
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-19 14:14:35 +1000

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,26 @@
+{
+    "format_version": 2,
+    "header": {
+        "name": "§sCosmic Teams§r",
+        "description": "§l§sCosmic Teams§r is a mod that lets users make teams.\n\n§oFeatures includes team chat, homes, teleporting, a combat system and more!",
+        "uuid": "64a50b0e-52a0-43a5-afcb-5e3aa3e5bdbc",
+        "version": [1, 0, 0],
+        "min_engine_version": [1, 20, 0]
+    },
+    "modules": [
+        {
+            "description": "Script module",
+            "type": "script",
+            "language": "javascript",
+            "uuid": "f5ecaf8b-8ad7-46d7-8cd6-d35b26160c43",
+            "version": [1, 0, 0],
+            "entry": "scripts/main.js"
+        }
+    ],
+    "dependencies": [
+        {
+            "module_name": "@minecraft/server",
+            "version": "2.7.0-beta"
+        }
+    ]
+}
